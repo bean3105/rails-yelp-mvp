b226ff6ba47da7336375e4d6fddcb92e562a1540
Georgia Bean-Butler <georgia.bean@icloud.com>
2023-08-23 14:26:03 +0100

    rails yelp app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: "Franco Manca", address: "123 London", category: "italian")
+Restaurant.create(name: "Four Regions", address: "345 Paris", category: "chinese")
+Restaurant.create(name: "Nobu", address: "123 New York", category: "japanese")
+Restaurant.create(name: "Gloria", address: "234 London", category: "italian")
+Restaurant.create(name: "Gigi", address: "234 Paris", category: "french")
